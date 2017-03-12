8fe673d83334633c0c10ad6327631479d86356d3
Jack Meyer <jack10042@gmail.com>
2017-03-12 07:01:24 -0400

    Added pull script

---
+++ download.sh
@@ -0,0 +1,6 @@
+sudo mkdir mov
+cd mov
+sudo curl -L -o basil.mp4 "https://drive.google.com/uc?export=download&id=0B-jg7yvnheHscjBHcjFPbHJCeG8"
+sudo curl -L -o main_build.mp4 "https://drive.google.com/uc?export=download&id=0B1FcPNOHo5e1cC1HR2EyQXBuV2M"
+sudo curl -L -o dome.mp4 "https://drive.google.com/uc?export=download&id=0B-jg7yvnheHscjBHcjFPbHJCeG8"
+sudo curl -L -o basilica.mp4 "https://drive.google.com/uc?export=download&id=0B-VQuHi41fF_bTRqaXp2OUU5MjQ" 
